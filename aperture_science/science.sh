753827c722476a1eb793bbe85612781daba7bcea
Michele Bucca <michele@lenovo>
2017-01-05 16:27:40 +0100

    Taught to the terminal how to sing like GlaDOS

---
+++ aperture_science/science.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+clear
+cat aperture_ascii.txt
+mplayer still_alive.mp3 > /dev/null 2>/dev/null & #184.9 seconds
+sleep 8
+echo This was a triumph
+sleep 4
+echo "I'm making a note here"
+sleep 2
+echo HUGE SUCCESS
+sleep 3
+echo "It's hard to overstate my satisfaction"
+sleep 7
+
+echo ""
+
+echo Aperture Science
+sleep 4
+echo we do what we must because we can
+sleep 5
+echo for the good of all of us except for the ones who are dead
+sleep 5.5
+echo "but there's no sense crying over every mistake"
+sleep 4.5
+echo you just keep on trying until you run out of cake
+sleep 4
+echo and the science gets done and you make a neat gun
+sleep 4
+echo for the people who are still alive
+sleep 9
+
+#Minute 1
+echo ""
+echo "I'm not even angry"
+sleep 4
+echo "I'm being so sincere right now"
+sleep 5
+echo even though you broke my heart and killed me
+sleep 7
+echo and tore me to pieces
+sleep 4
+echo and threw every piece into a fire
+sleep 5 
+echo as they burned it hurt because I was so happy for you!
+sleep 6
+echo Now these points of data make a beautyful line #91
+sleep 3.5
+echo "and we're out of beta, we're releasing on time"
+sleep 3.5
+echo "so I'm GLaD I got burned" 
+sleep 3 #101
+echo Think of all the things we learned for the people that are still alive
+sleep 11
+
+
+echo ""
+echo go ahead and leave me #112
+sleep 4
+echo I think I prefer to stay inside
+sleep 5
+echo "maybe you'll find someone else to help you"
+sleep 7
+echo maybe Black Mesa
+sleep 4
+echo "that was a joke, haha, fat chance"
+sleep 5
+echo "anyway this cake is great, it's so delicious and moist" #137
+sleep 5
+echo "look at me still talking, when there's science to do"
+sleep 4
+echo "when I look out there it makes me GLaD I'm not you"
+sleep 4 #150
+echo "I've experiments to run, there is research to be done"
+sleep  4
+echo on the people who are still alive
+sleep 4 #158
+
+echo ""
+echo and believe me I am still alive 
+sleep 4
+echo "I'm doing science and I'm still alive" #161
+sleep 5
+echo "I feel FANTASTIC and I'm still alive" #166
+sleep 4
+echo "While you are dying I'll be still alive" #171
+sleep 3
+echo "and when you're dead I'll be still alive" 
+sleep 4
+echo "STILL ALIVE, still alive "
+
